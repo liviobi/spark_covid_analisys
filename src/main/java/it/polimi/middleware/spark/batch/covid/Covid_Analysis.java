8c4e0967367e5b20f20d2e5661c2ff67489a2df4
liviobi <liviobiondo@gmail.com>
2021-07-12 18:05:10 +0200

    added caching

--- src/main/java/it/polimi/middleware/spark/batch/covid/Covid_Analysis.java
+++ src/main/java/it/polimi/middleware/spark/batch/covid/Covid_Analysis.java
@@ -18,7 +18,6 @@ import static org.apache.spark.sql.functions.*;
 
 
 public class Covid_Analysis {
-    private static final boolean useCache = true;
 
     public static void main(String[] args) {
         LogUtils.setLogLevel();
@@ -45,36 +44,51 @@ public class Covid_Analysis {
                                         to_date(col("dateRep"), "dd/MM/yyyy").as("date"),
                                         to_timestamp(col("dateRep"), "dd/MM/yyyy").as("timestamp"),
                                         col("countriesAndTerritories").as("country"),
-                                        col("cases").cast(DataTypes.IntegerType));
+                                        col("cases").cast(DataTypes.IntegerType)).cache();
 
         long days = 6 * 86400;
 
         cases_per_country_df = cases_per_country_df
                                 .withColumn("moving_avg", avg("cases")
-                                .over( Window.partitionBy("country").orderBy(col("timestamp").cast("long")).rangeBetween(-days, 0)));
+                                .over( Window.partitionBy("country").orderBy(col("timestamp").cast("long")).rangeBetween(-days, 0)))
+                                .cache()
+        ;
 
         //calcola dato aggregato
         Dataset<Row>  daily_cases_df = cases_per_country_df
                                         .groupBy("date","timestamp")
                                         .sum("cases")
                                         .orderBy("date")
-                                        .withColumnRenamed("sum(cases)","cases");
-        daily_cases_df = daily_cases_df.withColumn("moving_avg", avg("cases")
-                .over( Window.orderBy(col("timestamp").cast("long")).rangeBetween(-days, 0)));
+                                        .withColumnRenamed("sum(cases)","cases")
+                                        .cache()
+        ;
 
         daily_cases_df = daily_cases_df
-                                .withColumn("shifted_moving_avg",lag("moving_avg",1)
-                                .over(Window.orderBy("timestamp")));
+                                        .withColumn("moving_avg", avg("cases")
+                                        .over( Window.orderBy(col("timestamp").cast("long")).rangeBetween(-days, 0)))
+                                        .cache()
+        ;
+
+        daily_cases_df = daily_cases_df
+                                        .withColumn("shifted_moving_avg",lag("moving_avg",1)
+                                        .over(Window.orderBy("timestamp")))
+                                        .cache()
+        ;
+
         daily_cases_df = daily_cases_df.withColumn("percentage_increase",
                 when(
                         isnull(col("shifted_moving_avg")), 0)
-                        .otherwise(
-                                (col("shifted_moving_avg")
-                                        .minus(col("moving_avg"))
-                                        .divide(col("moving_avg")))
+                .otherwise(
+                        (col("shifted_moving_avg").minus(col("moving_avg")).divide(col("moving_avg")))
                         )
-        );
-        daily_cases_df = daily_cases_df.select("date","cases","moving_avg","percentage_increase");
+        )
+                .cache()
+        ;
+
+        daily_cases_df = daily_cases_df
+                                        .select("date","cases","moving_avg","percentage_increase")
+                                        .cache()
+        ;
 
 
         daily_cases_df
@@ -85,22 +99,32 @@ public class Covid_Analysis {
                 .csv(filePath + "files/covid/output/daily_cases.csv");
 
         daily_cases_df.show();
+        daily_cases_df.unpersist();
 
 
         WindowSpec window = Window.partitionBy("country").orderBy("timestamp");
-        cases_per_country_df = cases_per_country_df.withColumn("shifted_moving_avg",lag("moving_avg",1).over(window));
-        //rimuovi righe che contengono null values
+        cases_per_country_df = cases_per_country_df
+                                                    .withColumn("shifted_moving_avg",lag("moving_avg",1)
+                                                    .over(window))
+                                                    .cache()
+        ;
 
-        cases_per_country_df = cases_per_country_df.withColumn("percentage_increase",
-                                                    when(
-                                                            isnull(col("shifted_moving_avg")), 0)
-                                                    .otherwise(
-                                                            (col("shifted_moving_avg")
-                                                            .minus(col("moving_avg"))
-                                                            .divide(col("moving_avg")))
+        cases_per_country_df = cases_per_country_df
+                                                    .withColumn("percentage_increase",
+                                                        when(
+                                                                isnull(col("shifted_moving_avg")), 0)
+                                                        .otherwise(
+                                                                (col("shifted_moving_avg")
+                                                                        .minus(col("moving_avg"))
+                                                                        .divide(col("moving_avg")))
+                                                        )
                                                     )
-        );
-        cases_per_country_df = cases_per_country_df.select("date","country","cases","moving_avg","percentage_increase");
+                                                    .cache()
+        ;
+
+        cases_per_country_df = cases_per_country_df
+                                                    .select("date","country","cases","moving_avg","percentage_increase")
+                                                    .cache();
 
         cases_per_country_df
                 .coalesce(1)
@@ -115,8 +139,12 @@ public class Covid_Analysis {
         //select only meaningful data
         Dataset<Row> top_ten_df = cases_per_country_df.select(cases_per_country_df.col("*"), rank().over(window).alias("rank"))
                                             .filter(col("rank").leq(10))
-                                            .orderBy("date","rank");
-        top_ten_df = top_ten_df.select("date","country","percentage_increase","rank");
+                                            .orderBy("date","rank").cache();
+        top_ten_df = top_ten_df
+                                .select("date","country","percentage_increase","rank")
+                                .cache()
+        ;
+
         top_ten_df
                 .coalesce(1)
                 .write()
